c6235e12b2bb56fecf9eeaaf0bc2be4fdd2c67b2
aaleksandrov <aladex@gmail.com>
2023-11-29 10:24:19 +0100

    Update torrent save path before deleting it from qBittorrent.

--- qbittorrent/checker.go
+++ qbittorrent/checker.go
@@ -385,7 +385,13 @@ func addTorrentToQbittorrent(dbTorrent Torrent, sendTgMessage bool) bool {
 }
 
 func updateTorrentInQbittorrent(dbTorrent Torrent, torrentInfo models.Torrent) bool {
-	err := GlobalQbittorrentUser.DeleteTorrent(dbTorrent.Hash, false)
+	// Find save path of torrent
+	savePath, err := GlobalQbittorrentUser.GetDownloadPathByHash(dbTorrent.Hash)
+	if err == nil {
+		dbTorrent.SavePath = savePath
+	}
+
+	err = GlobalQbittorrentUser.DeleteTorrent(dbTorrent.Hash, false)
 	if err != nil {
 		log.Error("delete_torrent", err.Error(), nil)
 		return false
